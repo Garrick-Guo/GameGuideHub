bc19bd76a750fcf3e3c5870498c5e8b15b5578c2
Junhao Guo <guojunhao427@gmail.com>
2023-10-10 12:14:21 -0700

    return responses in JSON format

--- src/main/java/com/laioffer/twitch/hello/HelloController.java
+++ src/main/java/com/laioffer/twitch/hello/HelloController.java
@@ -1,2 +1,22 @@
-package com.laioffer.twitch.hello;public class HelloController {
+package com.laioffer.twitch.hello;
+
+import com.github.javafaker.Faker;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public Person sayHello() {
+        Faker faker = new Faker();
+        String name = faker.name().fullName();
+        String company = faker.company().name();
+        String street = faker.address().streetAddress();
+        String city = faker.address().city();
+        String state = faker.address().state();
+        String bookTitle = faker.book().title();
+        String bookAuthor = faker.book().author();
+        return new Person(name, company, new Address(street, city, state), new Book(bookTitle, bookAuthor));
+    }
+
 }
