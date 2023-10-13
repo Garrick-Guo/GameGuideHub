72d900a6451defa443ea5f89d8d83484a7481570
Junhao Guo <guojunhao427@gmail.com>
2023-10-12 22:46:58 -0700

    create appConfig serve as the Java version of application.yml

---
+++ src/main/java/com/laioffer/twitch/AppConfig.java
@@ -0,0 +1,32 @@
+package com.laioffer.twitch;
+
+import com.laioffer.twitch.external.TwitchApiClient;
+import com.laioffer.twitch.external.TwitchIdentityClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public TwitchIdentityClient twitchIdentityClient() {
+        WebClient client = WebClient.builder()
+                .baseUrl("https://id.twitch.tv/")
+                .build();
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).build();
+        return factory.createClient(TwitchIdentityClient.class);
+    }
+    @Bean
+    public TwitchApiClient twitchApiClient(@Value("${twithc.client-id}") String twitchClientId){
+        WebClient client = WebClient.builder()
+                .baseUrl("https://api.twitch.tv")
+                .defaultHeader("Client-Id", twitchClientId)
+                .build();
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).build();
+        return factory.createClient(TwitchApiClient.class);
+
+    }
+}
