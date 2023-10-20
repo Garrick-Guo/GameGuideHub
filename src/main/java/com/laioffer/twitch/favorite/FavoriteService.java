5388cb7372a8e7bc14a7a2f389ae3dd191815f4c
Junhao Guo <guojunhao427@gmail.com>
2023-10-20 15:12:39 -0700

    add recomemdation function

--- src/main/java/com/laioffer/twitch/favorite/FavoriteService.java
+++ src/main/java/com/laioffer/twitch/favorite/FavoriteService.java
@@ -6,6 +6,7 @@ import com.laioffer.twitch.db.entity.FavoriteRecordEntity;
 import com.laioffer.twitch.db.entity.ItemEntity;
 import com.laioffer.twitch.db.entity.UserEntity;
 import com.laioffer.twitch.model.TypeGroupedItemList;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,27 +15,43 @@ import java.util.List;
 
 @Service
 public class FavoriteService {
+
     private final ItemRepository itemRepository;
     private final FavoriteRecordRepository favoriteRecordRepository;
-    public FavoriteService(ItemRepository itemRepository, FavoriteRecordRepository favoriteRecordRepository) {
+
+    public FavoriteService(
+            ItemRepository itemRepository,
+            FavoriteRecordRepository favoriteRecordRepository
+    ) {
         this.itemRepository = itemRepository;
         this.favoriteRecordRepository = favoriteRecordRepository;
     }
 
-    @Transactional // make sure the separate db operation would either be commited or rolled back together
+    @CacheEvict(cacheNames = "recommend_items", key = "#root.args[0]")
+    @Transactional
     public void setFavoriteItem(UserEntity user, ItemEntity item) {
-        ItemEntity persistedItem = itemRepository.findByTwitchId(item.twitchId());
-        if (persistedItem == null) {
-            persistedItem = itemRepository.save(item);
+        ItemEntity peristedItem = itemRepository.findByTwitchId(item.twitchId());
+        if (peristedItem == null) {
+            peristedItem = itemRepository.save(item);
         }
-        FavoriteRecordEntity favoriteRecord = new FavoriteRecordEntity(null, user.id(), persistedItem.id(), Instant.now());
+        FavoriteRecordEntity favoriteRecord = new FavoriteRecordEntity(null, user.id(), peristedItem.id(), Instant.now());
         favoriteRecordRepository.save(favoriteRecord);
     }
+
+    @CacheEvict(cacheNames = "recommend_items", key = "#root.args[0]")
+    public void unsetFavoriteItem(UserEntity user, String twitchId) {
+        ItemEntity item = itemRepository.findByTwitchId(twitchId);
+        if (item != null) {
+            favoriteRecordRepository.delete(user.id(), item.id());
+        }
+    }
+
     public List<ItemEntity> getFavoriteItems(UserEntity user) {
         List<Long> favoriteItemIds = favoriteRecordRepository.findFavoriteItemIdsByUserId(user.id());
         return itemRepository.findAllById(favoriteItemIds);
     }
-    public TypeGroupedItemList getGroupedFavortieItems(UserEntity user) {
+
+    public TypeGroupedItemList getGroupedFavoriteItems(UserEntity user) {
         List<ItemEntity> items = getFavoriteItems(user);
         return new TypeGroupedItemList(items);
     }
